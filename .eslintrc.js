3a8fd24490ea891b962ee56c13d5d14df30293bb
Armand10-git <beyenemaxime10@gmail.com>
2026-07-18 22:32:29 +0100

    chore(s03): ci github actions + husky + commitlint + eslint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  env: {
+    node: true,
+    es2022: true,
+  },
+  rules: {
+    '@typescript-eslint/no-explicit-any': 'error',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+  },
+  ignorePatterns: ['dist/', 'build/', 'node_modules/', '*.js'],
+};
